dce3b94d90e61a8680a47d9647a63f7e7ea9ad55
David Collins <davidcollins4481@gmail.com>
2013-03-31 16:17:43 -0400

    change temp code to use new obj-oriented interface

--- homebot/homebot.py
+++ homebot/homebot.py
@@ -1,8 +1,17 @@
 from jabberbot import JabberBot, botcmd
 import datetime
-import temperature
+import subprocess
+from temperature_sensor import TemperatureSensor
 
 class SystemInfoJabberBot(JabberBot):
+
+    # fun
+    @botcmd
+    def greeting(self, mess, args):
+        """Greets you"""
+        return 'sup'
+
+    # system information
     @botcmd
     def serverinfo( self, mess, args):
         """Displays information about the server"""
@@ -10,26 +19,34 @@ class SystemInfoJabberBot(JabberBot):
         loadavg = open('/proc/loadavg').read().strip()
 
         return '%s\n\n%s' % ( version, loadavg, )
-    
+
+    @botcmd
+    def uptime(self, mess, args):
+        """Get current uptime information"""
+        return subprocess.check_output('uptime')
+
     @botcmd
     def time( self, mess, args):
         """Displays current server time"""
         return str(datetime.datetime.now())
 
     @botcmd
-    def rot13( self, mess, args):
-        """Returns passed arguments rot13'ed"""
-        return args.encode('rot13')
-
-    @botcmd
     def whoami(self, mess, args):
         """Tells you your username"""
         return mess.getFrom().getStripped()
 
+    # sensor information
     @botcmd
     def temperature(self, mess, args):
-        """Get the temperature of the room where the Raspberry Pi is located"""
-        return temperature.temperature()
+        """Get the temperature of the room where the Raspberry Pi is located in fahrenheit"""
+        sensor = TemperatureSensor()
+        return sensor.getTemperature()
+
+    @botcmd
+    def temperaturecelsius(self, mess, args):
+        """Get the temperature of the room where the Raspberry Pi is located in celsius"""
+        sensor = TemperatureSensor()
+        return sensor.getTemperatureCelsius()
 
 username = 'threefourfivepoplar@gmail.com'
 password = '###'
